4409d48217018074c6b05d070cdc90669b486a08
Saikat Bhadury <info2programmer@gmail.com>
2024-04-29 09:02:26 +0530

    Complex template using tailwind

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,4 @@
 <x-layout>
+    <x-slot:heading>About Us</x-slot:heading>
     <h1>Hello from the About Page</h1>
 </x-layout>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,12 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home page</title>
+    <title>{{$heading}} page</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
-    <x-nav></x-nav>
-    {{$slot}}
+    <div class="min-h-full">
+        <x-nav></x-nav>
+
+        <header class="bg-white shadow">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{$heading}}</h1>
+            </div>
+        </header>
+        <main>
+            <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+                {{$slot}}
+            </div>
+        </main>
+    </div>
+
 </body>
 
 </html>
